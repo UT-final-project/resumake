901e6e225511d224606a7307d436beff6b67e9c4
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-03-15 15:06:20 -0500

    Basic Setup for Express Server.
    Still need to init mongoose and express-session.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Start the API server
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
