446ec6e19237c1f2af3399c62db08d65d8583acb
Stephen Price <stephen_lprice@live.com>
2021-03-16 13:03:17 -0500

    added start, build, etc scripts

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/resumeDB");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/resumeDB");
 
 // Start the API server
 app.listen(PORT, function () {
