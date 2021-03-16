86821ba461058ef391770e12f7828bfc6bf35ac1
Stephen Price <stephen_lprice@live.com>
2021-03-16 13:20:44 -0500

    added morgan for logging

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
