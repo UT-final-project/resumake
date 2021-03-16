72d5ca0898d4a5d2ba293fcd6f676f2530e863de
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-03-16 17:44:40 -0500

    Got User Auth Working.
    I setup the user authentication. It will require a few things on the server side routes to get working when ever we set our routes up. I moved the routes file from the client side to the server side and added my test file there that can be used as a template. Also I added a user model but it was purely for test purposes and we can ignore it and build better models for the actual app. One more thing is I had to comment out the API.js in utils and index.js in routes because it was causing the server to crash. I think it is because none of these routes actually do anything yet.

--- server.js
+++ server.js
@@ -1,19 +1,55 @@
 const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const User = require("./models/userTest");
+const routes = require("./routes");
+// =============== END OF IMPORTS ============ //
 
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// =========== Define middleware here =========== //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Express-session, cors, passport and cookie parser for user authentication
+app.use(cors({
+    origin: "http://localhost:3000", // <-- location of the react app connecting to
+    credentials: true
+}))
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passportConfig")(passport);
+// =============== END OF MIDDLEWARE ============== //
+
+// app.use(routes);  <------ This can call all server side routes in the folder when we have any set up
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/resumeDB");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/resumeDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// },
+//     () => {
+//         console.log("Mongoose Is Connected");
+//     }
+// );
+
 
 // Start the API server
 app.listen(PORT, function () {
