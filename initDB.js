95192118ec189cc5179ecbf32035411e2e3a42e4
Stephen Price <stephen_lprice@live.com>
2021-03-17 11:12:08 -0500

    modified mongoose connection for an easier deployment

--- initDB.js
+++ initDB.js
@@ -9,13 +9,7 @@ module.exports = () => {
     throw envar.error;
   }
 
-  // Connecting to mongo database via environment variables
-  // Database, user and password must be created before running
-  connect(process.env.DB_URI, {
-    dbName: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    authSource: process.env.DB_NAME,
+  connect(process.env.MONGODB_URI || "mongodb://localhost/resumeDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
