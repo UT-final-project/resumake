b3ec2534c3a9e22848f6a2bd1aca63805a358f70
Dana Corona <dana.d.corona@gmail.com>
2021-03-18 10:32:48 -0500

    Updated photo height and updated the navbar name

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -6,7 +6,7 @@ function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand navbar-header" href="#">TRESemmé</a>
+                <a className="navbar-brand navbar-header" href="#">ResuMake</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
