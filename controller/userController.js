3c005f7606a272bdc4bf751e0541f34cf2e62277
Stephen Price <stephen_lprice@live.com>
2021-03-18 13:00:11 -0500

    removed duplicate create method

--- controller/userController.js
+++ controller/userController.js
@@ -36,12 +36,6 @@ module.exports = {
           .then(dbModel => res.status(200).json(dbModel))
           .catch(err => res.status(422).json(err));
       },
-      create: function(req, res) {
-        db.User
-          .create(req.body)
-          .then(dbModel => res.status(201).json(dbModel))
-          .catch(err => res.status(422).json(err));
-      },
       update: function(req, res) {
         db.User
           .findOneAndUpdate({ _id: req.params.id }, req.body)
