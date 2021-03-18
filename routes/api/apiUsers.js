1a2f089c757ff8e183ffda33738c1cc56de814fd
Stephen Price <stephen_lprice@live.com>
2021-03-18 12:51:23 -0500

    added api routes for users

--- routes/api/apiUsers.js
+++ routes/api/apiUsers.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 const userController = require("../../controller/userController");
 
-// User signup post route
+// Matches with "/api/users"
 router.route("/")
-    .post(userController.create);
+    // Responds with an array of all users
+    .get(userController.findAll)
+    // Adds a new user
+    .post(userController.create)
+
+// Matches with "/api/users/:id"
+router.route('/:id')
+  // Responds with a single user matching the provided ID
+  .get(userController.findById)
+  // Updates a user's properties matching the provided ID
+  .put(userController.update)
+  // Removes the user matching the provided ID
+  .delete(userController.remove)
 
 module.exports = router;
