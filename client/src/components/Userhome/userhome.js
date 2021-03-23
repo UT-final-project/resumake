8fd39119589be679e5a4859fccf6314ffd62f5fd
Dana Corona <dana.d.corona@gmail.com>
2021-03-23 12:15:54 -0500

    Updated Favicon, user home removed template, updated look

--- client/src/components/Userhome/userhome.js
+++ client/src/components/Userhome/userhome.js
@@ -17,42 +17,35 @@ function Userhome({ user, getUser, userLoggedIn }) {
 
     return (
         // Whole Container
-        <div className="container col-md-12">
+        <div className="container col-12">
             <div className="row">
-                <div className="col-md-12">
-                    {user ? (<h1 className="welcometext">Hi, {capitalize(user.firstname)} {capitalize(user.lastname)}!</h1>) :
-                        (<h1 className="welcometext">No User Logged In!</h1>)}
+                <div className="col-12">
+                    {user ? (<h1 className="welcometext d-flex justify-content-center">Hi, {capitalize(user.firstname)} {capitalize(user.lastname)}!</h1>) :
+                        (<h1 className="welcometext d-flex justify-content-center">No User Logged In!</h1>)}
                     <br />
                     <br />
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-4">
-                    <h2>Your Resume</h2>
-                    <img src="https://raw.githubusercontent.com/UT-final-project/group1-final-project/b6ffc1b8d66a71b17e8bdcb6221ea519585026db/client/svg-images/undraw_Resume_re_hkth.svg" alt="resume" className="userhomeimg" />
-                    <br />
-                    <br />
-                    <div className="row">
-                        <div className="col-md-6"><button type="button" className="btn userhomebtn editresume-btn">Edit Resume</button></div>
-                        <div className="col-md-6"><button type="button" className="btn userhomebtn download-btn">Download Resume</button></div>
+            <div className="container col-12 d-flex justify-content-around">
+                <div className="row">
+                    <div className="col-md-6 resumesection">
+                        <h2 d-flex justify-content-around>Your Resume</h2>
+                        <img src="https://raw.githubusercontent.com/UT-final-project/group1-final-project/67a757a95b943e9b8f3d76453b596c4549c7f236/client/svg-images/undraw_fill_form_re_cwyf.svg" alt="resume" className="userhomeimg" />
+                        <br />
+                        <br />
+                        <div className="row d-flex justify-content-around">
+                            <div className="col-6"><button type="button" className="btn userhomebtn editresume-btn">Edit Resume</button></div>
+                            <div className="col-6"><button type="button" className="btn userhomebtn download-btn">Download Resume</button></div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-4">
-                    <h2>Choose a Template</h2>
-                    <img src="https://raw.githubusercontent.com/UT-final-project/group1-final-project/f92e4abd48b13d8ffda7c872601d867ff3d34c39/client/svg-images/undraw_Reviewed_docs_re_9lmr.svg" alt="templates" className="userhomeimg" />
-                    <br />
-                    <br />
-                    <div className="row">
-                        <div className="col-md-12"><button type="button" className="btn userhomebtn edittemplate-btn">Edit Template</button></div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <h2>Your Webpage</h2>
-                    <img src="https://raw.githubusercontent.com/UT-final-project/group1-final-project/f92e4abd48b13d8ffda7c872601d867ff3d34c39/client/svg-images/undraw_Personal_site_re_c4bp.svg" alt="webpage" className="userhomeimg" />
-                    <br />
-                    <br />
-                    <div className="row">
-                        <div className="col-md-12"><button type="button" className="btn userhomebtn viewwebpage-btn">View Webpage</button></div>
+                    <div className="col-md-6 resumesection">
+                        <h2>View Resume</h2>
+                        <img src="https://raw.githubusercontent.com/UT-final-project/group1-final-project/5f481a4475ec3b4d454c996f4820d661a32abcb3/client/svg-images/undraw_Hiring_re_yk5n.svg" alt="webpage" className="userhomeimg" />
+                        <br />
+                        <br />
+                        <div className="row">
+                            <div className="col-12"><button type="button" className="btn userhomebtn viewwebpage-btn">View Resume</button></div>
+                        </div>
                     </div>
                 </div>
             </div>
