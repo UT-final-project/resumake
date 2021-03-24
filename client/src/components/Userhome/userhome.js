f6636ae6dc5e1a941ca8fe7d107da0ebcd0c3df2
Dana Corona <dana.d.corona@gmail.com>
2021-03-24 10:28:09 -0500

    removed download resume button

--- client/src/components/Userhome/userhome.js
+++ client/src/components/Userhome/userhome.js
@@ -41,8 +41,7 @@ function Userhome({ getUser }) {
                         <br />
                         <br />
                         <div className="row d-flex justify-content-around">
-                            <div className="col-6"><button type="button" className="btn userhomebtn editresume-btn"><FontAwesomeIcon icon="pen-fancy"/> Edit Resume</button></div>
-                            <div className="col-6"><button type="button" className="btn userhomebtn download-btn"><FontAwesomeIcon icon="save"/> Download Resume</button></div>
+                            <div className="col-12"><button type="button" className="btn userhomebtn editresume-btn"><FontAwesomeIcon icon="pen-fancy"/> Edit Resume</button></div>
                         </div>
                     </div>
                     <div className="col-md-6 resumesection">
